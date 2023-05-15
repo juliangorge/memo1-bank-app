6c7f065a0a09d07f2a4502fd70af44e7e6a83e74
Julián <juliangorge@hotmail.com>
2023-05-15 11:15:41 -0300

    Ajuste

--- src/main/java/com/aninfo/service/AccountService.java
+++ src/main/java/com/aninfo/service/AccountService.java
@@ -3,7 +3,9 @@ package com.aninfo.service;
 import com.aninfo.exceptions.DepositNegativeSumException;
 import com.aninfo.exceptions.InsufficientFundsException;
 import com.aninfo.model.Account;
+import com.aninfo.model.Transaction;
 import com.aninfo.repository.AccountRepository;
+import com.aninfo.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,9 @@ public class AccountService {
 
     @Autowired
     private AccountRepository accountRepository;
+
+    @Autowired
+    private TransactionService transactionService;
 
     public Account createAccount(Account account) {
         return accountRepository.save(account);
@@ -45,6 +50,8 @@ public class AccountService {
             throw new InsufficientFundsException("Insufficient funds");
         }
 
+        Transaction transaction = transactionService.createTransaction(new Transaction(cbu, sum));
+
         account.setBalance(account.getBalance() - sum);
         accountRepository.save(account);
 
@@ -65,6 +72,8 @@ public class AccountService {
         if (sum <= 0) {
             throw new DepositNegativeSumException("Cannot deposit negative sums");
         }
+
+        Transaction transaction = transactionService.createTransaction(new Transaction(cbu, sum));
 
         Account account = accountRepository.findAccountByCbu(cbu);
         account.setBalance(account.getBalance() + applyPromoIfApplicable(sum));
